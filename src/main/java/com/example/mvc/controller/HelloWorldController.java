4b23f85eaec586c4c2514d3dab9442b1b933148d
mfelczynski <mifelmi@gmail.com>
2023-04-24 21:23:54 +0200

    new modifications

---
+++ src/main/java/com/example/mvc/controller/HelloWorldController.java
@@ -0,0 +1,17 @@
+// HelloWorldController.java
+
+package com.example.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloWorldController {
+
+    @GetMapping("/hello")
+    public String helloWorld(Model model) {
+        model.addAttribute("message", "Hello, World!");
+        return "hello";
+    }
+}
